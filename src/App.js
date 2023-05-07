af2755729e6364ca259b91553378f66f9027ad9b
Tejas204 <tejasdhopavkar2000@gmail.com>
2023-05-07 16:45:47 +0530

    Added basic route structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import LandingPage from './Components/LandingPage';
+import NoMatch from './Components/NoMatch';
+import { Routes,Route } from 'react-router-dom';
+import LoginPage from './Components/LoginPage';
 
 function App() {
   return (
     <div>
-      <LandingPage/>
+      <Routes>
+        <Route path='/' element={<LandingPage/>}></Route>
+        <Route path='login' element={<LoginPage/>}></Route>
+        <Route path='*' element={<NoMatch/>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Components/NoMatch.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NoMatch = () => {
+  return (
+    <div>Thanks you for your patience! We are working towards building this page for you.</div>
+  )
+}
+
+export default NoMatch
