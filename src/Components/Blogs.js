74c974432b6360b869c23641ef11c00279d203e1
Tejas204 <tejasdhopavkar2000@gmail.com>
2023-06-14 23:02:55 +0530

    Added grid structure

--- src/Components/Blogs.js
+++ src/Components/Blogs.js
@@ -1,15 +1,57 @@
 import React from 'react'
 import VerticalNavbar from './VerticalNavbar'
+import mobile from '../Images/blog-website-mobile.png'
 
 const Blogs = () => {
   return (
-    // Grid structure for blogs page
-    <div className='grid grid-cols-4'>
-      {/* Vertical navbar column */}
-        <div className='col-span-1 md:col-span-1'>
+    // Grid structure for blogs
+    <div className='flex flex-row'>
+
+    {/* Vertical navbar column */}
+    <div className='grid grid-cols-1 w-[20%] fixed left-0'>
+        <div className='col-span-1'>
             <VerticalNavbar/>
         </div>
     </div>
+
+      <div className='grid grid-cols-3 w-[80%] relative left-[20%]'>
+              <div className='col-span-1 h-[100%]'>
+                <div className='p-4 border-2 rounded-lg'>
+                  <img src={mobile} className='h-[50%]'></img>
+                  <p>this is sample text</p>
+                </div>
+              </div>
+
+              <div className='col-span-1'>
+              <div className='p-4 border-2 rounded-lg'>
+                  <img src={mobile} className='h-[50%]'></img>
+                  <p>this is sample text</p>
+                </div>
+              </div>
+
+              <div className='col-span-1'>
+              <div className='p-4 border-2 rounded-lg'>
+                  <img src={mobile} className='h-[50%]'></img>
+                  <p>this is sample text</p>
+                </div>
+              </div>
+
+              <div className='col-span-1'>
+              <div className='p-4 border-2 rounded-lg'>
+                  <img src={mobile} className='h-[50%]'></img>
+                  <p>this is sample text</p>
+                </div>
+              </div>
+
+              <div className='col-span-1'>
+              <div className='p-4 border-2 rounded-lg'>
+                  <img src={mobile} className='h-[50%]'></img>
+                  <p>this is sample text</p>
+                </div>
+              </div>
+        </div>
+    </div> 
+    
   )
 }
 
