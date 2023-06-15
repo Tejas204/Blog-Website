11f32ee3df1ed0b4f8484dac50f1db8236cc797b
Tejas204 <tejasdhopavkar2000@gmail.com>
2023-06-15 22:51:24 +0530

    Added card

--- src/Components/Blogs.js
+++ src/Components/Blogs.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React from 'react';
 import VerticalNavbar from './VerticalNavbar'
 import mobile from '../Images/blog-website-mobile.png'
+import VR from '../Images/VR.jpg';
+import Cyber from '../Images/Cyber.jpg';
+import Blockchain from '../Images/Blockchain.jpg';
 
 const Blogs = () => {
   return (
@@ -14,13 +17,41 @@ const Blogs = () => {
         </div>
     </div>
 
-      <div className='grid grid-cols-3 w-[80%] relative left-[20%]'>
-              <div className='col-span-1 h-[100%]'>
-                <div className='p-4 border-2 rounded-lg'>
-                  <img src={mobile} className='h-[50%]'></img>
-                  <p>this is sample text</p>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-[80%] relative left-[20%]'>
+        {/******* Card begins ********/}
+              <div className='col-span-1 p-10'>
+                <div className='border-2 shadow-lg rounded-md'>
+                  {/* Title image of the blog */}
+                  <img src={VR} className='h-[50%] rounded-t-md'></img>
+
+                  {/* This div contains the date, title text, like option and rating */}
+                  {/* Parent div */}
+                  <div className='flex flex-col gap-y-3 p-2'>
+
+                    {/* Date */}
+                    <p className=' text-sm md:text-md text-slate-500'>
+                      20-Apr-2023
+                    </p>
+
+                    {/* Blog title and help text */}
+                    <p className='text-sm md:text-md font-bold'>
+                      Cybersecurity: The great wall of online protection
+                    </p>
+                    <p className='text-sm text-slate-500 text-justify'>
+                      Cybersecurity is the practice of protecting systems, networks, and programs from digital attacks. 
+                      These cyberattacks are usually aimed at accessing, changing, or destroying sensitive information.
+                    </p>
+
+                    {/* View button */}
+                    <div className='flex justify-center'>
+                      <button className='p-3 bg-[#fccf47] text-[#1a1a1d] font-semibold w-[25%] rounded-md'>
+                        View
+                      </button>
+                    </div>
+                  </div>
                 </div>
               </div>
+        {/******* Card ends ********/}
 
               <div className='col-span-1'>
               <div className='p-4 border-2 rounded-lg'>
