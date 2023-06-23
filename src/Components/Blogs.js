f8928075f0f2b20d4a284eb0845aeec2f1ea3584
Tejas204 <tejasdhopavkar2000@gmail.com>
2023-06-23 22:17:31 +0530

    Added categories and sorting options

--- src/Components/Blogs.js
+++ src/Components/Blogs.js
@@ -21,15 +21,25 @@ const Blogs = () => {
         {/* Create button, category dropdown, sorting dropdown */}
         {/* Options div */}
         <div className='flex flex-row absolute top-0 left-[20%] p-3 w-[80%] justify-between'>
-              {/* 1. Category dropdown */}  
-              <button className='p-2 bg-[#fccf47] text-[#1A1A1D] font-semibold rounded-lg hover:ring-2 hover:ring-offset-2 hover:ring-[#fccf47] ease-in-out duration-200 shadow-md'>
-                Create blog
-              </button>
+              {/* 1. Category dropdown */}
+              <select className='p-2 bg-[#1a1a1d] text-[#ffffff] font-normal rounded-lg hover:ring-2 hover:ring-offset-2 hover:ring-[#1a1a1d] ease-in-out duration-200 shadow-md' name='category'>
+                <option value='all_categories'>All Categories</option>
+                <option value='technology'>Technology</option>
+                <option value='food'>Food</option>
+                <option value='tourism'>Tourism</option>
+              </select>
 
-                {/* Remove filter button */}
-                <button className='p-2 bg-[#fccf47] text-[#1A1A1D] font-semibold rounded-lg hover:ring-2 hover:ring-offset-2 hover:ring-[#fccf47] ease-in-out duration-200 shadow-md'>
-                  Remove filter
-                </button>
+              
+
+                {/* Sorting dropdown button */}
+                <select className='p-2 bg-[#1a1a1d] text-[#ffffff] font-normal rounded-lg hover:ring-2 hover:ring-offset-2 hover:ring-[#1a1a1d] ease-in-out duration-200 shadow-md'>
+                  <option value='sort_a_z'>Sort by A-Z</option>
+                  <option value='sort_z_a'>Sort by Z-A</option>
+                  <option value='sort_date_latest'>Sort by Date (latest)</option>
+                  <option value='sort_date_oldest'>Sort by Date (old)</option>
+                  <option value='sort_least_read_time'>Least reading time</option>
+                  <option value='sort_most_read_time'>Most reading time</option>
+                </select>
           </div>
 
 
