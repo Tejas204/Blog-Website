33413b4e85256ad903db6452768654c5df5b1c7d
Tejas Dhopavkar <tejasdhopavkar2000@gmail.com>
2023-08-30 21:57:09 +0530

    Added section to add a comment

--- src/Components/ViewBlog.js
+++ src/Components/ViewBlog.js
@@ -20,8 +20,8 @@ const ViewBlog = () => {
           * Subdivision 2 -  contains category and reading time */}
         <div className={columnTwo}>
 
-            {/* Subdivision 1 */}
-            <div className='w-[100%] col-span-4 md:col-span-3 h-screen p-4 space-y-2'>
+            {/* Subdivision 1 - Blog */}
+            <div className='w-[100%] col-span-4 md:col-span-3 h-screen p-8 space-y-2'>
                 {/* Blog title */}
                 <p className='text-lg md:text-xl lg:text-2xl font-semibold text-left'>Blockchain: The ledger needed for the modern world</p>
 
@@ -57,7 +57,7 @@ const ViewBlog = () => {
                     <p className='fontp-semibold text-lg md:text-xl lg:text-2xl'>Comments</p>
 
                     {/* Comments */}
-                    <div className='space-y-2'>
+                    <div className='space-y-5'>
                         <div className='p-2 border-2 border-[#1a1a1d] rounded-lg hover:bg-slate-300 hover:border-slate-500 ease-in-out duration-200'>
                             <p className='font-semibold'>Tejas | {new Date().toLocaleDateString()}</p>
                             <p className='text-justify text-gray-800'>This is a great work on ethereum. This helped me a lot in my project. Keep sharing such articles. This is a great work on ethereum. This helped me a lot in my project. Keep sharing such articles. This is a great work on ethereum. This helped me a lot in my project. Keep sharing such articles</p>
@@ -73,11 +73,20 @@ const ViewBlog = () => {
                             <p className='text-justify text-gray-800'>This is a great work on ethereum. This helped me a lot in my project. Keep sharing such articles. This is a great work on ethereum. This helped me a lot in my project. Keep sharing such articles. This is a great work on ethereum. This helped me a lot in my project. Keep sharing such articles</p>
                         </div>
                     </div>
+
+                    {/* Div for adding a comment */}
+                    <div>
+                        {/* Div for comment area */}
+                        <div className='w-[100%]'>
+                            <textarea rows='4' className='w-[100%] border-2 border-[#1a1a1d] rounded-lg p-2 focus:ring-2 focus:ring-[#1a1a1d] ease-in-out duration-150' placeholder='Share your thoughts..'></textarea>
+                        </div>
+                        {/* Div for post button */}
+                    </div>
                     
                 </div>
             </div>
 
-            {/* Subdivision 2 */}
+            {/* Subdivision 2 - Blog information*/}
             <div className='space-y-4 w-[20%] md:col-span-1 hidden md:block h-screen bg-slate-200 text-left p-4 fixed right-0'>
                 {/* Category */}
                 <p className='border-b-2 border-[#1a1a1d] text-[#1a1a1d] text-xl font-semibold'>Category</p>
@@ -89,7 +98,7 @@ const ViewBlog = () => {
 
                 {/* Related blogs*/}
                 <p className='border-b-2 border-[#1a1a1d] text-[#1a1a1d] text-xl font-semibold'>Related blogs</p>
-                <div className='space-y-2 overflow-y-auto h-3/5 scroll-smooth'>
+                <div className='space-y-4 overflow-y-auto h-3/5 scroll-smooth'>
                 {blogsCards.map((blogCard, index) => {
                     if(blogCard.category == 'technology'){
                         return(
